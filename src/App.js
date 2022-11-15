0a27ef3545278e4119419c4fa1687d255407ad58
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-11-15 17:30:04 -0300

    feat: add search pokemon in searchbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Searchbar from "./components/Searchbar";
+import { searchPokemon } from "./api";
 
 function App() {
+
+  const onSearchHandler = async (pokemon) => {
+    const pokemonData = await searchPokemon(pokemon);
+    console.log("pokemonData: ", pokemonData);
+  }
   return (
     <div>
       <Navbar />
-      <Searchbar />
+      <Searchbar
+      onSearch={onSearchHandler}
+      />
       <div className="App"></div>
     </div>
   );
